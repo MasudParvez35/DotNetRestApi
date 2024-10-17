108adcddec44e45b9e2c995d845cfc5d7b10d67e
Parvezbs1538 <masud.parvez@brainstation-23.com>
2024-10-17 08:44:26 +0600

    Get By action name

--- DotNetRestApi/Controllers/ValuesController.cs
+++ DotNetRestApi/Controllers/ValuesController.cs
@@ -1,24 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetRestApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        [Route("Get")]
         [HttpGet]
         public string GetName()
         {
             return "Parvez";
         }
 
-        [Route("GetFullName")]
         [HttpGet]
         public string GetFullName()
         {
-            return "Masud Parvez";
+            return "A S M Masud Parvez";
         }
     }
 }
