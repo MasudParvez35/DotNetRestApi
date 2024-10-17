5271766bfcc10b8e1a306f2b4e3bfc71c642e289
Parvezbs1538 <masud.parvez@brainstation-23.com>
2024-10-17 09:20:35 +0600

    insert data using postman

---
+++ DotNetRestApi/Controllers/PostController.cs
@@ -0,0 +1,37 @@
+﻿using DotNetRestApi.Data;
+using DotNetRestApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetRestApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Post> GetAll()
+        {
+            var posts = _context.Posts.ToList();
+            return posts;
+        }
+
+        [HttpPost]
+        public Post Add(Post post)
+        {
+            post.Title = "Test2";
+            post.CreatedDate = DateTime.Now;
+            _context.Posts.Add(post);
+            bool isSaved = _context.SaveChanges() > 0;
+            if (isSaved)
+                return post;
+            return null;
+        }
+    }
+}
---
+++ DotNetRestApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DotNetRestApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetRestApi.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; }
+}
