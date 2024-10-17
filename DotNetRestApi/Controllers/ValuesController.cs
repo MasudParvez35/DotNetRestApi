2df45de260555ec849d9c4148cf52736c3f4f6a6
Parvezbs1538 <masud.parvez@brainstation-23.com>
2024-10-17 08:34:12 +0600

    Add project files.

---
+++ DotNetRestApi/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("Get")]
+        [HttpGet]
+        public string GetName()
+        {
+            return "Parvez";
+        }
+
+        [Route("GetFullName")]
+        [HttpGet]
+        public string GetFullName()
+        {
+            return "Masud Parvez";
+        }
+    }
+}
